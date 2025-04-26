f8d04a4ba9a4328652c6e42bb6cd0c46187d3dae
KopralProject <kopralwann04@gmail.com>
2025-04-26 17:26:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   TOKEN: '8010600412:AAH54PzbTV0TeRwt1SPC40zzgjGlE9LRt0w',
   SELLER_CODE: '13245',
-  OWNER_NAME: 'Bang Kopral',
-  OWNER_USERNAME: '@Bangkopral',
-  OWNER_ID: '6904837223',
-  MONGO_DB: 'mongodb://kopralwann.04@gmail.com:Kopral13245',
+  OWNER_NAME: 'Sonzai X シ',
+  OWNER_USERNAME: 'November2k',
+  OWNER_ID: '679008122',
+  MONGO_DB: 'https://cloud.mongodb.com/v2/680caec7a397025ad6aa25ec',
   DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
 };
